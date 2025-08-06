6380ee3e92dbdec21dc3d56a661316cd4712a94e
levinadevx <anna_levina@internet.ru>
2025-08-06 19:37:36 +0300

    build: добавлен nginx

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'backend', '172.18.0.4']
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
