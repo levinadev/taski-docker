a6daf3a1808da7c4d77caeeea371af46ad57447b
Anna Levina <anna_levina@internet.ru>
2026-03-31 14:12:31 +0300

    chore: add 31.177.82.106 in ALLOWED_HOSTS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'backend', 'annahost.zapto.org']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'backend', 'annahost.zapto.org', '31.177.82.106']
 
 # Application definition
 
